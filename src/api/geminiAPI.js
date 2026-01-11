80ef87d74e8d0a024f9dca3ff6cad33c38de5ebe
piyush-baniya <baniyapiyush0810@gmail.com>
2026-01-11 20:05:59 +0545

    google gemini api setup

---
+++ src/api/geminiAPI.js
@@ -0,0 +1,14 @@
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({
+  apiKey: import.meta.env.VITE_GOOGLE_GEMINI_API_KEY,
+});
+
+export const generateContent = async (prompt) => {
+  const result = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: prompt,
+  });
+  console.log(result.text);
+  return () => result.text; // return the response
+};
